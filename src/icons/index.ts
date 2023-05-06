d73f0ad4fd8634a6848cff2cc9434ec60642bb4d
Jhoncel Nayve <jhoncelnayve@gmail.com>
2023-05-06 11:38:42 +0000

    Create Spinner component.

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -3,3 +3,4 @@ export type { default as IconProps } from "./iconProps";
 export { default as Star } from "./Star";
 export { default as Heart } from "./Heart";
 export { default as MagnifyingGlass } from "./MagnifyingGlass";
+export { default as Spinner } from "./Spinner";
